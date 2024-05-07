624fff9451ad90c322fa23264687c8bdc68810ea
abdusss111 <abdusalamabdu29@gmail.com>
2024-05-07 11:15:06 +0500

    Initial Commit

---
+++ EventApp/EventAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventAppApp.swift
+//  EventApp
+//
+//  Created by Abdussalam Abdurakhimov on 07.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct EventAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
